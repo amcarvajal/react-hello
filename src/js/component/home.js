6a9304ff17837f4dae2510f39d479057f91df6e2
Miguel Angel Carvajal Gonzalez <amcarvajal15@gmail.com>
2021-05-07 17:08:40 +0000

    Traffic ligth Done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,50 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../../styles/index.scss";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	//const [ color, setColor] = useState("red");
+	const [btnClass, setBtnClass] = useState("");
+	const [btnClassYellow, setBtnClassYellow] = useState("");
+	const [btnClassGreen, setBtnClassGreen] = useState("");
+	// RED
+	const handleClick = () => {
+		setBtnClass("selected");
+		setBtnClassYellow("");
+		setBtnClassGreen("");
+		console.log(btnClass);
+	};
+	//YELLOW
+	const handleClickYellow = () => {
+		setBtnClassYellow("selected");
+		setBtnClassGreen("");
+		setBtnClass("");
+		console.log(btnClassYellow);
+	};
+	// GREEN
+	const handleClickGreen = () => {
+		setBtnClassGreen("selected");
+		setBtnClassYellow("");
+		setBtnClass("");
+		console.log(btnClassGreen);
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="trafficTops"></div>
+			<div className="container">
+				<div
+					className={`redLight ${btnClass}`}
+					onClick={handleClick}></div>
+				<div
+					className={`yellowLight ${btnClassYellow}`}
+					onClick={handleClickYellow}></div>
+				<div
+					className={`greenLight ${btnClassGreen}`}
+					onClick={handleClickGreen}></div>
+			</div>
 		</div>
 	);
 }
